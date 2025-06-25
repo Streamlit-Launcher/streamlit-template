f78063ac20b1900bcb77aebe1ff9aee961d61d6b
Joseph Ampfer <149810620+joseph-ampfer@users.noreply.github.com>
2025-06-25 18:39:38 +0000

    Version 4.0

--- demo-app/hello.py
+++ demo-app/hello.py
@@ -15,7 +15,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="Hello", page_icon=":material/waving_hand:")
-st.title("Welcome to Streamlit! 👋  -> Edit 2.0")
+st.title("Welcome to Streamlit! 👋  -> Version 4.0")
 st.write(
     """
     Streamlit asdfadsf is an open-source app framework built specifically for
